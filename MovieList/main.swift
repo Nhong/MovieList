7845fb914bf1fd7f269bac7824427d53b61f1c23
kittinun chobthum <chobtham.kittinun@gmail.com>
2020-04-04 15:43:27 +0700

    Initial Commit

---
+++ MovieList/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MovieList
+//
+//  Created by Kittinun Chobtham on 4/4/2563 BE.
+//  Copyright © 2563 Kittinun Chobtham. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
